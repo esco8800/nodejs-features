aa972dfc9b43992c3f55f05c0ff6df3324fc55d3
ilya.semenov <ilya.semenov@chulakov.ru>
2021-05-20 14:59:39 +0300

    Правка сохранения файла

--- stream/file/gzipReceive.js
+++ stream/file/gzipReceive.js
@@ -1,18 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 const zlib = require('zlib');
+const {join: joinPath} = require('path');
 
 const server = http.createServer((req, res) => {
     const filename = req.headers.filename;
-    console.log('File request received: ' + filename);
+    const filepath = joinPath(__dirname, 'output/' + filename);
+    console.log('File path: ' + filepath);
 
     req
         .pipe(zlib.createGunzip())
-        .pipe(fs.createWriteStream(filename))
+        .pipe(fs.createWriteStream(filepath))
         .on('finish', () => {
             res.writeHead(201, {'Content-Type': 'text/plain'});
             res.end("That's it\n");
-            console.log(`File saved: ${filename}`);
+            console.log(`File saved: ${filepath}`);
         });
 });
 
